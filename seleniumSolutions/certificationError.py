fd22f1f2e8d233db40ebfe7cace692c25248947a
EnverJ <Enlem1987@gmail.com>
2024-05-16 23:55:48 -0600

    certification error

---
+++ seleniumSolutions/certificationError.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+from selenium.webdriver.chrome.service import Service as ChromeService
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver.implicitly_wait(10)
+driver.get("https://the-internet.herokuapp.com/")
