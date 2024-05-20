124d8e76b2165716208210b9446ad2a7c7f2b8a8
EnverJ <Enlem1987@gmail.com>
2024-05-20 00:00:06 -0600

    option

--- seleniumSolutions/certificationError.py
+++ seleniumSolutions/certificationError.py
@@ -1,10 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver import ActionChains
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 from selenium.webdriver.chrome.service import Service as ChromeService
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+
+options = Options()
+options.add_argument("--allow-running-insecure-content")
+options.add_argument("--ignore-certificate-errors")
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install(),options=options))
 driver.implicitly_wait(10)
-driver.get("https://the-internet.herokuapp.com/")
+
+driver.get("https://expired.badssl.com/")
+time.sleep(5)
