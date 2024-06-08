bc04674ebdd6a971c5255cd06c7b7ac45cba9472
EnverJ <Enlem1987@gmail.com>
2024-06-07 22:14:07 -0600

    pytest How to run all tests or specific tests using PyTest

---
+++ Pytest/test_demo3.py
@@ -0,0 +1,17 @@
+import pytest
+
+
+def test_m4():
+    assert False
+
+
+def test_m5():
+    assert 100 == 100
+
+
+def test_m6():
+    assert "enver" == "Enver"
+
+
+def test_login():
+    assert "admin" == "admin"
