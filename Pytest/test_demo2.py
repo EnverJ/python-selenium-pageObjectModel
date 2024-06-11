f2eda33a47649a8a85c2007943234a6bd64bc889
EnverJ <Enlem1987@gmail.com>
2024-06-10 23:47:44 -0600

    added some contents

--- Pytest/test_demo2.py
+++ Pytest/test_demo2.py
@@ -19,3 +19,10 @@ def test_m6():
 @pytest.mark.login
 def test_login():
     assert "admin" == "admin"
+
+
+# 1. pip install pytest pytest-xdist selenium
+# 2. pytest -m login or py.test -m login Pytest/test_demo2.py
+# To run tests with parallel execution, you can combine the -n option from pytest-xdist:
+# 3. pytest -m login -n 4
+
